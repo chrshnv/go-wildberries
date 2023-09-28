74920b7e603b102d3b2a89ce661a738eec77c0de
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2023-09-28 20:54:47 +0500

    http.Client initialization and auth token auto-completion to http.Client

---
+++ transport.go
@@ -0,0 +1,17 @@
+package go_wildberries
+
+import "net/http"
+
+type WildberriesAuthTransport struct {
+	transport http.RoundTripper
+	token     string
+}
+
+func (t *WildberriesAuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("Authorization", t.token)
+	return t.transport.RoundTrip(req)
+}
+
+func NewWildberriesAuthTransport(token string) *WildberriesAuthTransport {
+	return &WildberriesAuthTransport{transport: http.DefaultTransport, token: token}
+}
--- wildberries.go
+++ wildberries.go
@@ -1,4 +1,11 @@
 package go_wildberries
 
+import "net/http"
+
 type WildberriesAPI struct {
+	client http.Client
+}
+
+func NewWildberriesAPI(auth string) *WildberriesAPI {
+	return &WildberriesAPI{client: http.Client{Transport: NewWildberriesAuthTransport(auth)}}
 }
