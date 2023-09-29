559b06052cdade0715ec45c32596ce5e9514143c
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2023-09-29 16:23:45 +0500

    'CreatedDate' format fix

--- feedbacks/models.go
+++ feedbacks/models.go
@@ -1,18 +1,20 @@
 package feedbacks
 
-import "time"
+import (
+	"github.com/chrshnv/go-wildberries/responses"
+)
 
 type WildberriesFeedbackListResponse struct {
 	CountUnanswered int `json:"countUnanswered"`
 	CountArchive    int `json:"countArchive"`
 	Feedbacks       []struct {
-		ID               string    `json:"ID"`
-		Username         string    `json:"userName"`
-		MatchingSize     string    `json:"matchingSize"`
-		Text             string    `json:"text"`
-		ProductValuation int       `json:"productValuation"`
-		CreatedDate      time.Time `json:"createdDate"`
-		State            string    `json:"state"`
+		ID               string                    `json:"ID"`
+		Username         string                    `json:"userName"`
+		MatchingSize     string                    `json:"matchingSize"`
+		Text             string                    `json:"text"`
+		ProductValuation int                       `json:"productValuation"`
+		CreatedDate      responses.CreatedDateTime `json:"createdDate"`
+		State            string                    `json:"state"`
 		Answer           struct {
 			Text  string `json:"text"`
 			State string `json:"state"`
