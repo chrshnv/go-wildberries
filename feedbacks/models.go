12938dba155e8bb74ebdaafaba086475c886257a
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2023-10-02 15:09:38 +0500

    privikni k apparatu

--- feedbacks/models.go
+++ feedbacks/models.go
@@ -1,20 +1,20 @@
 package feedbacks
 
 import (
-	"github.com/chrshnv/go-wildberries/responses"
+	"time"
 )
 
 type WildberriesFeedbackListResponse struct {
 	CountUnanswered int `json:"countUnanswered"`
 	CountArchive    int `json:"countArchive"`
 	Feedbacks       []struct {
-		ID               string                    `json:"ID"`
-		Username         string                    `json:"userName"`
-		MatchingSize     string                    `json:"matchingSize"`
-		Text             string                    `json:"text"`
-		ProductValuation int                       `json:"productValuation"`
-		CreatedDate      responses.CreatedDateTime `json:"createdDate"`
-		State            string                    `json:"state"`
+		ID               string    `json:"ID"`
+		Username         string    `json:"userName"`
+		MatchingSize     string    `json:"matchingSize"`
+		Text             string    `json:"text"`
+		ProductValuation int       `json:"productValuation"`
+		CreatedDate      time.Time `json:"createdDate"`
+		State            string    `json:"state"`
 		Answer           struct {
 			Text  string `json:"text"`
 			State string `json:"state"`
