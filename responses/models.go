ec89e6a293536ecc3a45ca84b33777b1a1a41e97
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2023-10-02 16:09:32 +0500

    clean

--- responses/models.go
+++ responses/models.go
@@ -1,33 +1,8 @@
 package responses
 
-import (
-	"strings"
-	"time"
-)
-
 type WildberriesResponse[T any] struct {
 	Data             T        `json:"data"`
 	Error            bool     `json:"error"`
 	ErrorText        string   `json:"errorText"`
 	AdditionalErrors []string `json:"additionalErrors"`
-}
-
-type CreatedDateTime time.Time
-
-func (c *CreatedDateTime) UnmarshalJSON(b []byte) error {
-	value := strings.Trim(string(b), `"`) //get rid of "
-	if value == "" || value == "null" {
-		return nil
-	}
-
-	t, err := time.Parse(time.RFC3339, value) //parse time
-	if err != nil {
-		return err
-	}
-	*c = CreatedDateTime(t) //set result using the pointer
-	return nil
-}
-
-func (c CreatedDateTime) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + time.Time(c).Format(time.RFC3339) + `"`), nil
 }
